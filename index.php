26737525b77e1b3f9fe444140b4d07e5ce1f5929
lovbackan <simon@lovbacka.se>
2023-01-08 20:17:43 +0100

    started with front-end

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require("./hotelFunctions.php");
 
 <head>
     <link href="styles.css" rel="stylesheet" />
+    <link href="fonts.css" rel="stylesheet" />
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -18,39 +19,50 @@ require("./hotelFunctions.php");
 
     <header>
         <nav>
-            <a href="./index.php">Home</a>
-            <a href="./standard.php">Standard</a>
-            <a href="./economy.php">Economy</a>
-            <a href="./luxury.php">Luxury</a>
+            <h2>Groundbreaker</h2>
+            <div class="navInfo">
+                <a href="./index.php">Home</a>
+                <a href="./standard.php">Standard</a>
+                <a href="./economy.php">Economy</a>
+                <a href="./luxury.php">Luxury</a>
+            </div>
         </nav>
     </header>
+    <main>
+        <section class="hero">
+            <img class="heroDesktop" src="/pictures/hero-home.jpg" />
+            <div class="heroInfo">
+                <h2>Welcome to the Groundbreaker</h2>
+                <p>A hotel among the stars</p>
+                <button class="buy">Make reservation</button>
+            </div>
+        </section>
 
-    <h1>Under Construction!</h1>
-    <h2>30% discount if you stay longer than one night!</h2>
-    <form id="inputForm" method="post">
-        <label for="transferCode">Transfer Code</label>
-        <input type="text" name="transferCode" id="transferCode" required class="form-control">
-        <label for="name">Arrival</label>
-        <input type="date" name="arrival" id="arrival" required class="form-control" min="2023-01-01" max="2023-01-31">
-        <label for="name">Departure</label>
-        <input type="date" name="departure" id="departure" required class="form-control" min="2023-01-01" max="2023-01-31">
-        <label for="room" class="form-control">Room</label>
-        <select required name=room id="room">
-            <option disabled selected value>Select a room!</option>
-            <option value="Economy">Economy</option>
-            <option value="Standard">Standard</option>
-            <option value="Luxury">Luxury</option>
-        </select>
-        <label for="totalCost" class="form-control">Total Cost</label>
-        <input type="text" name="totalCost" id="totalCost" readonly>
-        <button type="submit">Book!</button>
-        <label for="offer1" class="form-control">Offer 1</label>
-        <input type="checkbox" id="offer1" name="options[]" value="1" />
-        <label for="offer2" class="form-control">Offer 2</label>
-        <input type="checkbox" id="offer2" name="options[]" value="2" />
-        <label for="offer3" class="form-control">Offer 3</label>
-        <input type="checkbox" id="offer3" name="options[]" value="3" />
-    </form>
+        <form id="inputForm" method="post">
+            <label for="transferCode">Transfer Code</label>
+            <input type="text" name="transferCode" id="transferCode" required class="form-control">
+            <label for="name">Arrival</label>
+            <input type="date" name="arrival" id="arrival" required class="form-control" min="2023-01-01" max="2023-01-31">
+            <label for="name">Departure</label>
+            <input type="date" name="departure" id="departure" required class="form-control" min="2023-01-01" max="2023-01-31">
+            <label for="room" class="form-control">Room</label>
+            <select required name=room id="room">
+                <option disabled selected value>Select a room!</option>
+                <option value="Economy">Economy</option>
+                <option value="Standard">Standard</option>
+                <option value="Luxury">Luxury</option>
+            </select>
+            <label for="totalCost" class="form-control">Total Cost</label>
+            <input type="text" name="totalCost" id="totalCost" readonly>
+            <button type="submit">Book!</button>
+            <label for="offer1" class="form-control">Offer 1</label>
+            <input type="checkbox" id="offer1" name="options[]" value="1" />
+            <label for="offer2" class="form-control">Offer 2</label>
+            <input type="checkbox" id="offer2" name="options[]" value="2" />
+            <label for="offer3" class="form-control">Offer 3</label>
+            <input type="checkbox" id="offer3" name="options[]" value="3" />
+        </form>
+    </main>
 </body>
 
 </html>
