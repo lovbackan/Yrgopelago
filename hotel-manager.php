04cd2c219bbafd9558dfaa9a4d3ea92efc4045f1
Josef Forkman <strand.vatten@outlook.com>
2023-01-17 13:50:38 +0100

    Create hotel-manager.php

---
+++ hotel-manager.php
@@ -0,0 +1,36 @@
+<?php
+require("./hotelFunctions.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="styles.css" rel="stylesheet" />
+    <link href="fonts.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="/vendor/benhall14/php-calendar/html/css/calendar.css">
+    <script defer src="./script.js"></script>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Groundbreaker</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="./index.php">
+                <h1>Groundbreaker</h1>
+            </a>
+            <div class="navInfo">
+                <a href="./roomPages/economy.php">Economy</a>
+                <a href="./roomPages/standard.php">Standard</a>
+                <a class="lastNavItem" href="./roomPages/luxury.php">Luxury</a>
+            </div>
+        </nav>
+    </header>
+    <main>
+        
+    </main>
+</body>
+
+</html>
